989aa4fc30958ff8e0234183d9e40f32613edb88
lelisvaldo@outlook.com <almeida85416346>
2020-03-23 20:51:01 -0300

    Estritura final do projto EX2 - Concluido EX3 - Concluido EX4 - Concluido

--- Village/src/ini4/Ini4.java
+++ Village/src/ini4/Ini4.java
@@ -1,7 +1,28 @@
 package ini4;
 
+import javax.swing.*;
+
 public class Ini4 {
     public void exec(){
+        String vI = JOptionPane.showInputDialog(null,"Entre com a valor 'A'.");
+        int a = Integer.parseInt(vI);
 
+        String vF = JOptionPane.showInputDialog(null,"Entre com a valor 'B'.");
+        int b = Integer.parseInt(vF);
+
+        System.out.print(returnSumImpar(a, b));
+    }
+
+    private int returnSumImpar(int vI, int vF){
+        int i = 0;
+        int resultSum = 0;
+
+        for (i = vI; i<=vF; i++){
+            //System.out.print(i);
+            if (i % 2 == 1){
+                resultSum += i;
+            }
+        }
+        return resultSum;
     }
 }
